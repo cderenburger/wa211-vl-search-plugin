7c6464b4fd525113f7708d8bd8c3d7179e475f63
Cory Derenburger <cory@derenburgersolutions.com>
2020-01-06 21:44:52 -0500

    comment out RH taxonomy code

--- public/js/win211-search-plugin-public.js
+++ public/js/win211-search-plugin-public.js
@@ -56,29 +56,29 @@ $(document).ready(function(){
 			.each(function(){
 			this.href = "https://wa211.communityos.org/searchresults/render/ds/" +domain1+$(this).attr('data-relativeurl')+"%2C%22__react_key%22%3A%22ta_id_"+$(this).attr('data-relativeurl')+domain2+zip+domain3;
 			});
-			$("a[href^='https://www.resourcehouse.info/win211/Search/Topics/']").not("a[href^='https://www.resourcehouse.info/win211/CHANGEME']")
-			.each(function(){
-			 if ($(this).attr('data-relativeurl').indexOf('?') > -1)
-				{
-					this.href = "https://www.resourcehouse.info/win211/Search/Topics/"+$(this).attr('data-relativeurl')+amp+"loc="+location+amp+"geo="+geocoor+amp+"reg="+region+amp+"o=distance-asc";
-				} else
-				{
-					this.href = "https://www.resourcehouse.info/win211/Search/Topics/"+$(this).attr('data-relativeurl')+"?"+amp+"loc="+location+amp+"geo="+geocoor+amp+"reg="+region+amp+"o=distance-asc";
-				}
-			});
+			//$("a[href^='https://www.resourcehouse.info/win211/Search/Topics/']").not("a[href^='https://www.resourcehouse.info/win211/CHANGEME']")
+			//.each(function(){
+			// if ($(this).attr('data-relativeurl').indexOf('?') > -1)
+			//	{
+			//		this.href = "https://www.resourcehouse.info/win211/Search/Topics/"+$(this).attr('data-relativeurl')+amp+"loc="+location+amp+"geo="+geocoor+amp+"reg="+region+amp+"o=distance-asc";
+			//	} else
+			//	{
+			//		this.href = "https://www.resourcehouse.info/win211/Search/Topics/"+$(this).attr('data-relativeurl')+"?"+amp+"loc="+location+amp+"geo="+geocoor+amp+"reg="+region+amp+"o=distance-asc";
+			//	}
+		//	});
 		},
 		minLength:1
 	};
-	var ac_keyword = {
-		source: citiespath+"php/211search_keywords.php",
-		select: function(event, ui){
-			$("#taxname").val(ui.item.taxval);
-			var taxonomyname = $("#taxval").val();
-			var amp = "&".replace(/#038;/g,"");
-		console.log(taxname);
-		},
-		minLength:1
-	};
+	//var ac_keyword = {
+	//	source: citiespath+"php/211search_keywords.php",
+	//	select: function(event, ui){
+	//		$("#taxname").val(ui.item.taxval);
+	//		var taxonomyname = $("#taxval").val();
+	//		var amp = "&".replace(/#038;/g,"");
+	//	console.log(taxname);
+	//	},
+	//	minLength:1
+//	};
 		$("#city").autocomplete(ac_cities);
 		$("#taxname").autocomplete(ac_keyword);
 });
